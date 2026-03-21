d254bcdfebb9b929d95b267ec91d60c4c136d0e3
Natan <natanoficial48@gmail.comn>
2026-03-21 03:27:56 -0300

    fix admin login

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -12,8 +12,7 @@ router = APIRouter(prefix="/auth", tags=["Auth"])
 def register(user: UserCreate, db: Session = Depends(get_db)):
     existing_user = db.query(User).filter(User.email == user.email).first()
     if existing_user:
-        raise HTTPException(status_code=400, detail="Email já cadastrado")
-
+        raise HTTPException(status_code=400, detail="Email ja cadastrado")
     new_user = User(
         name=user.name,
         email=user.email,
@@ -21,24 +20,19 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
         role=user.role,
         restaurant_id=user.restaurant_id
     )
-
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
-
-    return {"message": "Usuário criado com sucesso"}
+    return {"message": "Usuario criado com sucesso"}
 
 @router.post("/login")
 def login(data: UserLogin, db: Session = Depends(get_db)):
     user = db.query(User).filter(User.email == data.email).first()
-
     if not user or not verify_password(data.password, user.password):
-        raise HTTPException(status_code=401, detail="Credenciais inválidas")
-
+        raise HTTPException(status_code=401, detail="Credenciais invalidas")
     token = create_token({
         "user_id": user.id,
         "restaurant_id": user.restaurant_id,
         "role": user.role
     })
-
     return {"access_token": token}
